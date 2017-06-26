ffd9920b96633b0adafb7767532f8617136f461c
n0de_coyote <nelsonandrewwells@gmail.com>
2017-06-26 14:30:44 -0700

    Add basic widget.

---
+++ app/src/main/java/com/node_coyote/bakerscorner/RecipeWidget.java
@@ -0,0 +1,51 @@
+package com.node_coyote.bakerscorner;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.node_coyote.bakerscorner.recipes.RecipeActivity;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class RecipeWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        Intent intent = new Intent(context, RecipeActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
+        views.setTextViewText(R.id.widget_recipe_name_text_view, widgetText);
+        
+        views.setOnClickPendingIntent(R.id.widget_recipe_name_text_view, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
