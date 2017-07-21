35da23823d1322d40dafb5106e63dd26e83964d9
n0de_coyote <nelsonandrewwells@gmail.com>
2017-07-21 10:16:34 -0700

    Espresso test for Brownies.

--- app/src/androidTest/java/com/node_coyote/bakerscorner/RecipeActivityScreenTest.java
+++ app/src/androidTest/java/com/node_coyote/bakerscorner/RecipeActivityScreenTest.java
@@ -1,7 +1,8 @@
 package com.node_coyote.bakerscorner;
 
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.ViewMatchers;
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,14 +12,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by node_coyote on 6/26/17.
@@ -29,13 +28,47 @@ public class RecipeActivityScreenTest {
 
     @Rule
     public ActivityTestRule<RecipeActivity> testRule = new ActivityTestRule<>(RecipeActivity.class);
+//
+//    @Rule
+//    public ActivityTestRule activityRule = new ActivityTestRule<>(
+//            SpecialActivity.class,
+//            true,    // initialTouchMode
+//            false);  //Lazy launching
+//
+//    @Test
+//    public void specialTest() {
+//        Intent intent = new Intent();
+//        Bundle bundle = new Bundle();
+//        bundle.putString(SpecialActivity.KEY_SPECIAL_VALUE, "789");
+//        intent.putExtra(SpecialActivity.ARG_SPECIAL_BUNDLE, bundle);
+//        activityRule.launchActivity(intent);
+//
+//        onView(withId(R.id.special))
+//                .check(matches(withText("789")));
+//    }
+
+//    @Rule
+//    public ActivityTestRule<RecipeActivity> mActivityRule =
+//            new ActivityTestRule<RecipeActivity>(RecipeActivity.class) {
+//                @Override
+//                protected Intent getActivityIntent() {
+//                    Context targetContext = InstrumentationRegistry.getInstrumentation()
+//                            .getTargetContext();
+//                    Intent result = new Intent(targetContext, RecipeActivity.class);
+//                    result.getLongExtra("ROW_ID", 1);
+//                    return result;
+//                }
+//            };
+
 
     @Test
     public void clickRecipe_OpensRecipeDetailActivity() {
-        //onData(anything()).inAdapterView(withId(R.id.recipe_item_container)).atPosition(1).perform(click());
-        onView(ViewMatchers.withId(R.id.recipe_recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-        //onView(allOf(withId(R.id.recipe_title_view),withText("Brownies"))).perform(click());
-        //onView(withId(R.id.recipe_title_view)).check(matches(withText("Nutella Pie")));
+
+        onView(withId(R.id.recipe_recycler_view))
+                .check(matches(hasDescendant(withText("Brownies"))));
+
+        onView(withId(R.id.recipe_recycler_view))
+                .check(matches(hasDescendant(withId(R.id.recipe_item_container)))).perform(click());
+
     }
 }
