d11972fc6755dcc10873ffc569ecd15f9fee69cd
n0de_coyote <nelsonandrewwells@gmail.com>
2017-07-30 22:02:48 -0700

    Remove old player code.

--- app/src/main/java/com/node_coyote/bakerscorner/steps/StepsCursorAdapter.java
+++ app/src/main/java/com/node_coyote/bakerscorner/steps/StepsCursorAdapter.java
@@ -3,8 +3,8 @@ package com.node_coyote.bakerscorner.steps;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +12,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.exoplayer2.ExoPlaybackException;
-import com.google.android.exoplayer2.ExoPlayer;
-import com.google.android.exoplayer2.Timeline;
-import com.google.android.exoplayer2.source.TrackGroupArray;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.node_coyote.bakerscorner.R;
-import com.node_coyote.bakerscorner.recipes.RecipeDetailActivity;
 import com.node_coyote.bakerscorner.steps.StepContract.StepEntry;
 import com.squareup.picasso.Picasso;
 
@@ -60,11 +54,11 @@ public class StepsCursorAdapter extends RecyclerView.Adapter<StepsCursorAdapter.
 
         if (!thumbnail.isEmpty() && thumbnail.length() != 0) {
             Picasso.with(holder.mThumbnailView.getContext()).load(thumbnail).into(holder.mThumbnailView);
-            holder.mThumbnailView.setVisibility(View.VISIBLE);
         }
 
-
-        holder.mDescriptionView.setText(description);
+        if (!TextUtils.isEmpty(description)) {
+            holder.mDescriptionView.setText(description);
+        }
 
         holder.mPlayVideoButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,13 +67,6 @@ public class StepsCursorAdapter extends RecyclerView.Adapter<StepsCursorAdapter.
                 mContext.startActivity(intent);
             }
         });
-
-//        if (!video.isEmpty() || video.length() != 0) {
-//            holder.initializePlayer(Uri.parse(video));
-//            holder.mExoPlayerView.setVisibility(View.VISIBLE);
-//        } else {
-//            holder.mExoPlayerView.setVisibility(View.GONE);
-//        }
     }
 
     @Override
@@ -93,84 +80,17 @@ public class StepsCursorAdapter extends RecyclerView.Adapter<StepsCursorAdapter.
         notifyDataSetChanged();
     }
 
-    class StepsCursorViewHolder extends RecyclerView.ViewHolder implements ExoPlayer.EventListener {
+    class StepsCursorViewHolder extends RecyclerView.ViewHolder {
 
         final TextView mDescriptionView;
         final ImageView mThumbnailView;
         final Button mPlayVideoButton;
-//        final SimpleExoPlayerView mExoPlayerView;
-//        SimpleExoPlayer mExoPlayer;
 
         StepsCursorViewHolder(View itemView) {
             super(itemView);
-//            mExoPlayerView = (SimpleExoPlayerView) itemView.findViewById(R.id.recipe_exo_player);
             mThumbnailView = (ImageView) itemView.findViewById(R.id.step_detail_list_item_image_view);
             mDescriptionView = (TextView) itemView.findViewById(R.id.step_detail_list_item_text_view);
             mPlayVideoButton = (Button) itemView.findViewById(R.id.step_detail_list_item_play_button);
-//            mExoPlayer = null;
-        }
-
-        /**
-//         * Initialize ExoPlayer.
-//         * @param mediaUri The URI of the sample to play.
-//         */
-//        private void initializePlayer(Uri mediaUri) {
-//            if (mExoPlayer == null) {
-//                // Create an instance of the ExoPlayer.
-//                TrackSelector trackSelector = new DefaultTrackSelector();
-//                LoadControl loadControl = new DefaultLoadControl();
-//                mExoPlayer = ExoPlayerFactory.newSimpleInstance(mContext, trackSelector, loadControl);
-//                Log.v("PLAYER", String.valueOf(mExoPlayerView));
-//                mExoPlayerView.setPlayer(mExoPlayer);
-//
-//                // Set the ExoPlayer.EventListener to this activity.
-//                mExoPlayer.addListener(this);
-//
-//                // Prepare the MediaSource.
-//                // TODO WTF
-//                String userAgent = Util.getUserAgent(mContext, "WTF");
-//                MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
-//                        mContext, userAgent), new DefaultExtractorsFactory(), null, null);
-//                mExoPlayer.prepare(mediaSource);
-//                mExoPlayer.setPlayWhenReady(false);
-//            }
-//        }
-//
-//        public void releasePlayer() {
-//            if (mExoPlayer != null) {
-//                mExoPlayer.release();
-//                mExoPlayer = null;
-//            }
-//        }
-
-        @Override
-        public void onTimelineChanged(Timeline timeline, Object manifest) {
-
-        }
-
-        @Override
-        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
-
-        }
-
-        @Override
-        public void onLoadingChanged(boolean isLoading) {
-
-        }
-
-        @Override
-        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-
-        }
-
-        @Override
-        public void onPlayerError(ExoPlaybackException error) {
-
-        }
-
-        @Override
-        public void onPositionDiscontinuity() {
-
         }
     }
 }
